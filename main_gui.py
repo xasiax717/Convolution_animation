cb0d7594607ade4ecb2b5abb1fe21b6ccd1cb2da
zgoreck1 <01178642@pw.edu.pl>
2024-01-20 16:54:33 +0100

    dodanie animacji do gui

--- main_gui.py
+++ main_gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from trytry import conv_plot, tri_plot
+from animatedPlot import AnimatedPlot
 
 class App:
     def __init__(self, master):
@@ -13,13 +14,13 @@ class App:
         frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 
-        frame_conv = ttk.Frame(self.master)
-        frame_conv.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        function_figure_conv = conv_plot()
+        #frame_conv = ttk.Frame(self.master)
+        #frame_conv.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        #function_figure_conv = conv_plot()
 
-        canvas_conv = FigureCanvasTkAgg(function_figure_conv, frame_conv)
-        canvas_widget_conv = canvas_conv.get_tk_widget()
-        canvas_widget_conv.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        #canvas_conv = FigureCanvasTkAgg(function_figure_conv, frame_conv)
+        #canvas_widget_conv = canvas_conv.get_tk_widget()
+        #canvas_widget_conv.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         frame_tri = ttk.Frame(self.master)
         frame_tri.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
@@ -27,7 +28,15 @@ class App:
 
         canvas_tri = FigureCanvasTkAgg(function_figure_tri, frame_tri)
         canvas_widget_tri = canvas_tri.get_tk_widget()
-        canvas_widget_tri.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        canvas_widget_tri.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        frame_animation = ttk.Frame(self.master)
+        frame_animation.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        #Parametry na razie nie działają i zawsze sie rysuje splot trojkata z kwadratem
+        AnimatedPlot(frame_animation, "tri", "sqr", 1, 2)
+
+
 
 
 if __name__ == "__main__":
