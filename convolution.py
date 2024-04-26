9902576866fa42606956d6f184174920488ec8ac
Zuzanna Górecka <01178642@pw.edu.pl>
2024-04-27 00:19:43 +0200

    everything's fine

--- convolution.py
+++ convolution.py
@@ -7,23 +7,15 @@ import matplotlib.pyplot as plt
 # "Cosinus": ["amplitude", "frequency", "phase"],
 # "Exponential": ["amplitude", "rate"]
 
-def square_wave(t, T):
-    return np.where(np.mod(t, T) < T / 2, 1, 0)
 
-def square_wave_non_periodic(t, amplitude, shift, width):
-    return np.where(np.abs(t-shift) < width / 2, amplitude, 0)
+def square_wave(t, amplitude, shift, width):
+    return np.where(np.abs(t - shift) < width / 2, amplitude, 0)
 
-def triangle_wave(t, T):
-    return np.abs((t / (T / 2)) % 2 - 1)
-
-def triangle_wave_non_periodic(t, T):
-    return np.where(np.abs(t)< T / 2, np.abs((t / (T / 2)) % 2 - 1), 0)
+def triangle_wave(t, amplitude, shift, width):
+    return np.where(np.abs(t-shift)< width / 2, amplitude*np.abs((t / (width / 2)) % 2 - 1), 0)
 
 def exponential_wave(t, tau):
     return np.where(t>0, np.exp(-t / tau), 0)
-
-def unit_step(t):
-    return np.heaviside(t, 1)
 
 def sinusoidal_wave(t, A, f, φ):
     return A * np.sin(2 * np.pi * f * t + φ)
@@ -46,16 +38,10 @@ def get_convolution_data(signal1_choice, signal2_choice, dt):
         signal1 = triangle_wave(t, T=2)
     elif signal1_choice == 3:
         signal1 = exponential_wave(t, tau=1)
-    elif signal1_choice == 4:
-        signal1 = unit_step(t)
     elif signal1_choice == 5:
         signal1 = sinusoidal_wave(t, A=1, f=1)
     elif signal1_choice == 6:
         signal1 = cosinusoidal_wave(t, A=1, f=1)
-    elif signal1_choice == 7:
-        signal1 = square_wave_non_periodic(t, amplitude=2, shift=1, width=2)
-    elif signal1_choice == 8:
-        signal1 = triangle_wave_non_periodic(t, T=2)
     else:
         raise ValueError("Invalid choice for signal 1.")
 
@@ -65,16 +51,10 @@ def get_convolution_data(signal1_choice, signal2_choice, dt):
         signal2 = triangle_wave(t, T=2)
     elif signal2_choice == 3:
         signal2 = exponential_wave(t, tau=1)
-    elif signal2_choice == 4:
-        signal2 = unit_step(t)
     elif signal2_choice == 5:
         signal2 = sinusoidal_wave(t, A=1, f=1)
     elif signal2_choice == 6:
         signal2 = cosinusoidal_wave(t, A=1, f=1)
-    elif signal2_choice == 7:
-        signal2 = square_wave_non_periodic(t, amplitude=2, width=2, shift=5)
-    elif signal2_choice == 8:
-        signal2 = triangle_wave_non_periodic(t, T=2)
     else:
         raise ValueError("Invalid choice for signal 2.")
 
@@ -120,21 +100,12 @@ def main():
         elif choice == 3:
             selected_signals.append(exponential_wave(t, tau=1))
             labels.append('Exponential Wave')
-        elif choice == 4:
-            selected_signals.append(unit_step(t))
-            labels.append('Unit Step')
         elif choice == 5:
             selected_signals.append(sinusoidal_wave(t, A=2, f=0.5, φ=0))
             labels.append('Sinusoidal Wave')
         elif choice == 6:
             selected_signals.append(cosinusoidal_wave(t, A=3, f=0.1, phase=0))
             labels.append('Cosinusoidal Wave')
-        elif choice == 7:
-            selected_signals.append(square_wave_non_periodic(t, amplitude=1, shift=1, width=1))
-            labels.append('Square Wave')
-        elif choice == 8:
-            selected_signals.append(triangle_wave_non_periodic(t, T=2))
-            labels.append('Triangle Wave')
         else:
             print("Invalid choice. Please enter a number between 1 and 8.")
 
